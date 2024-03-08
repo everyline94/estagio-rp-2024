6fedf93ca21214d020603aac810c7038bc51cbab
bruno larocca <brunolujanlarocca@gmail.com>
2024-03-08 18:36:06 -0300

    exercicio 3

---
+++ src/Exercicio3.java
@@ -0,0 +1,19 @@
+public class Exercicio3 {
+
+    /*
+
+    a) Progressão aritmética com razão 2, o próximo elemento é 9
+
+    b) Progressão aritmetica  onde cada termo é o dobro do anterior, o próximo elemento é 128
+
+    c) Sequencia de quadrados perfeitos, cada termino é o quadrado do número que apresenta sua posição na sequencia, o próximo valor é 49
+
+    d) Sequencia de quadrados de números pares começando de 2 e aumentando em 2 a cada passom o próximo na sequencia é 100
+
+    e) Sequencia de fibonacci, onde cada termo é a soma dos dois termos anteriores, o próximo na sequencia é 13
+
+    f) Combinação de progressão aritmetica e elementos que são aumentados em uma unidadem o próximo elemento é 20
+
+
+     */
+}
